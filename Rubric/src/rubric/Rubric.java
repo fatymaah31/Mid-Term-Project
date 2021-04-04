355f77721b65eb59514bd6092ff87f13071863b5
Fatima Mujahid <fatymaah31@gmail.com>
2021-04-04 16:31:44 +0500

    Mid Term Project
    It is a Mid Term Project of Rubrics Management

---
+++ Rubric/src/rubric/Rubric.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rubric;
+
+/**
+ *
+ * @author fatym
+ */
+public class Rubric {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Mainmenu Frame1 = new Mainmenu();
+        Frame1.setVisible(true);
+    }
+    
+}
